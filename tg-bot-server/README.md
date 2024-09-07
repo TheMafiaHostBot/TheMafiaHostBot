997a9efb94f9e3065f94e62d090149ee2943f81d
Hewston Fox <hewstonfox@gmail.com>
2024-09-07 16:39:55 +0300

    feat: add setup guide

--- tg-bot-server/README.md
+++ tg-bot-server/README.md
@@ -1,5 +1,33 @@
 ### TODO
+
 * Will require patched teloxide
 * Usa facades for telegram related work
 * Game related commands redirects to the correct or random game-server
 * Updates database with telegram related stuff (users, groups)
+
+# The Mafia Host Bot
+
+## Setup for development
+
+1. Install [ngrok](https://ngrok.com/docs/getting-started/)
+2. Setup `ngrok` [static domain](https://dashboard.ngrok.com/cloud-edge/domains)
+3. [Create a bot](https://core.telegram.org/bots/features#creating-a-new-bot) for development
+4. Setup environment by copying `.env.example` to `.env`
+    1. Update `TELOXIDE_TOKEN` value with your bot token from [@BotFather](https://t.me/BotFather)
+    2. Update `WEBHOOK_URL` value with domain created in ngrok (`https://<domain>`)
+
+## Running
+
+1. Run `ngrok` with `PORT` from `.env` and [static domain](https://dashboard.ngrok.com/cloud-edge/domains)
+
+```shell
+ngrok http <PORT> --domain=<domain>
+```
+
+2. Run the bot
+
+```shell
+cargo run
+```
+
+To restart the bot just rerun last command, ngrok not requires restarting
--- tg-bot-server/src/main.rs
+++ tg-bot-server/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::{prelude::*, update_listeners::webhooks};
+
+const ENV_PORT: &str = "PORT";
+const WEBHOOK_URL: &str = "WEBHOOK_URL";
+
+fn get_env(env: &'static str) -> String {
+    std::env::var(env).unwrap_or_else(|_| panic!("Cannot get the {env} env variable"))
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().unwrap();
+    pretty_env_logger::init();
+
+    log::info!("Starting ngrok ping-pong bot...");
+
+    let bot = Bot::from_env();
+
+    let port = get_env(ENV_PORT);
+    let url = get_env(WEBHOOK_URL).parse().unwrap();
+
+    let addr = format!("0.0.0.0:{port}").parse().unwrap();
+    let mut options = webhooks::Options::new(addr, url);
+
+    async fn root() -> &'static str {
+        "Hello, World!"
+    }
+
+    options.drop_pending_updates = true;
+    options.extra_routes.push(("/extra", axum::routing::get(root)));
+
+    let listener = webhooks::axum(bot.clone(), options)
+        .await
+        .expect("Couldn't setup webhook");
+
+    teloxide::repl_with_listener(
+        bot,
+        |bot: Bot, msg: Message| async move {
+            bot.send_message(msg.chat.id, "pong").await?;
+            Ok(())
+        },
+        listener,
+    )
+        .await;
 }
